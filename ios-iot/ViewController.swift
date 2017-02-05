7c35812b9c2af6f0e3d22d81c1b90cbb17356873
James Sturtevant <jsturtevant@gmail.com>
2017-02-04 20:43:38 -0500

    use mqqt to connect to azure iot-hub

--- ios-iot/ViewController.swift
+++ ios-iot/ViewController.swift
@@ -7,13 +7,29 @@
 //
 
 import UIKit
+import CocoaMQTT
 
 class ViewController: UIViewController {
-
+    var mqtt: CocoaMQTT?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        simpleSSLSetting()
     }
+
+    func simpleSSLSetting() {
+        let clientID = "workshopdevice"
+        mqtt = CocoaMQTT(clientID: clientID, host: "<your-iothub-name>.azure-devices.net", port: 8883)
+        mqtt!.username = "<your-iothub-name>>.azure-devices.net/<devicename>"
+        mqtt!.password = "SharedAccessSignature yourshared-sas-key"
+        mqtt!.willMessage = CocoaMQTTWill(topic: "/will", message: "dieout")
+        mqtt!.keepAlive = 60
+        mqtt!.delegate = self
+        mqtt!.enableSSL = true
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -22,6 +38,73 @@ class ViewController: UIViewController {
  
     @IBAction func connectbtn(_ sender: Any) {
         print("connect")
+        
+        mqtt!.connect()
     }
 }
 
+// from sample at https://github.com/emqtt/CocoaMQTT/tree/master/Example
+extension ViewController: CocoaMQTTDelegate {
+    func mqtt(_ mqtt: CocoaMQTT, didConnect host: String, port: Int) {
+        print("didConnect \(host):\(port)")
+    }
+    
+    // Optional ssl CocoaMQTTDelegate
+    func mqtt(_ mqtt: CocoaMQTT, didReceive trust: SecTrust, completionHandler: @escaping (Bool) -> Void) {
+        /// Validate the server certificate
+        ///
+        /// Some custom validation...
+        ///
+        /// if validatePassed {
+        ///     completionHandler(true)
+        /// } else {
+        ///     completionHandler(false)
+        /// }
+        completionHandler(true)
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didConnectAck ack: CocoaMQTTConnAck) {
+        print("didConnectAck: \(ack)，rawValue: \(ack.rawValue)")
+        
+        if ack == .accept {
+            
+        }
+        
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didPublishMessage message: CocoaMQTTMessage, id: UInt16) {
+        print("didPublishMessage with message: \(message.string)")
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didPublishAck id: UInt16) {
+        print("didPublishAck with id: \(id)")
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didReceiveMessage message: CocoaMQTTMessage, id: UInt16 ) {
+        print("didReceivedMessage: \(message.string) with id \(id)")
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didSubscribeTopic topic: String) {
+        print("didSubscribeTopic to \(topic)")
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didUnsubscribeTopic topic: String) {
+        print("didUnsubscribeTopic to \(topic)")
+    }
+    
+    func mqttDidPing(_ mqtt: CocoaMQTT) {
+        print("didPing")
+    }
+    
+    func mqttDidReceivePong(_ mqtt: CocoaMQTT) {
+        _console("didReceivePong")
+    }
+    
+    func mqttDidDisconnect(_ mqtt: CocoaMQTT, withError err: Error?) {
+        _console("mqttDidDisconnect")
+    }
+    
+    func _console(_ info: String) {
+        print("Delegate: \(info)")
+    }
+}
