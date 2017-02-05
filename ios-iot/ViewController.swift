e67aead2c52c4c78d7f6377d3edc8122727408b8
James Sturtevant <jsturtevant@gmail.com>
2017-02-04 20:13:08 -0500

    initial commit

---
+++ ios-iot/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ios-iot
+//
+//  Created by James Sturtevant on 2/4/17.
+//  Copyright © 2017 James Sturtevant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+ 
+    @IBAction func connectbtn(_ sender: Any) {
+        print("connect")
+    }
+}
+
